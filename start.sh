24ad9c9b9f4ae9b613d9add18280b01bb6a6fb3e
Reinaldo Jorge <reinaldosampa@gmail.com>
2025-07-23 22:49:05 -0300

    Corrige nginx para servir pasta hls com CORS

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@
 service nginx start
 
 ffmpeg -i "rtsp://veritron:Intelbras1@veritron.ddns-intelbras.com.br:554/cam/realmonitor?channel=1&subtype=1" \
--c:v copy -c:a copy -f hls -hls_time 2 -hls_list_size 5 -hls_flags delete_segments /var/www/html/hls/stream.m3u8
+-c:v copy -c:a copy -f hls -hls_time 2 -hls_list_size 5 -hls_flags delete_segments /var/www/html/hls/stream.m3u8 &
+
+nginx -g "daemon off;"
