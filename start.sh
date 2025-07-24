2d5b29e03c1c0f18063ca657ec51ab9539d7f4a5
Reinaldo Jorge <reinaldosampa@gmail.com>
2025-07-24 00:29:44 -0300

    Aumenta analyzeduration e probesize

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 service nginx start
 
-# Mude a URL abaixo pela sua RTSP se necessário
-ffmpeg -i "rtsp://veritron:Intelbras1@veritron.ddns-intelbras.com.br:554/cam/realmonitor?channel=1&subtype=1" \
+ffmpeg -analyzeduration 10000000 -probesize 10000000 \
+-i "rtsp://veritron:Intelbras1@veritron.ddns-intelbras.com.br:554/cam/realmonitor?channel=1&subtype=1" \
 -c:v copy -c:a aac -f hls -hls_time 2 -hls_list_size 5 -hls_flags delete_segments \
 /var/www/html/hls/stream.m3u8
